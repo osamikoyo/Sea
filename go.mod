7400e155d92377ca8a65d397c088cf2f5603d093
osamikoyo <v.pylayev@mail.ru>
2024-12-17 06:17:30 +0500

    ascii output

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module sea
 
 go 1.23.1
 
-require github.com/BurntSushi/toml v1.4.0 // indirect
+require (
+	github.com/BurntSushi/toml v1.4.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/rs/zerolog v1.33.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+)
--- internal/app/app.go
+++ internal/app/app.go
@@ -4,16 +4,18 @@ import (
 	"fmt"
 	"os"
 	"sea/internal/directory"
+	"sea/internal/loger"
 	"sea/internal/templates/parser"
 	"sea/internal/tomltools"
 )
 
 func Run(args []string) {
+	logger := loger.New()
 	switch args[1] {
 	case "search":
 		body, err := directory.GetTempl(os.Args[2])
 		if err != nil {
-			fmt.Println(err)
+			logger.Error().Err(err)
 		}
 
 		templ, err := tomltools.Get(body)
@@ -22,11 +24,21 @@ func Run(args []string) {
 		}
 
 		if err = parser.Pars(templ, os.Args[3], false); err != nil {
-			fmt.Println(err)
+			logger.Error().Err(err)
 		}
+		fmt.Println(`
+  ####   ######    ##
+ #       #        #  #
+  ####   #####   #    #
+      #  #       ######
+ #    #  #       #    #
+  ####   ######  #    #
+
+`)
+		logger.Info().Msg("Template " + os.Args[2] + " parsed successfully")
 	case "create":
 		if err := directory.Create(); err != nil {
-			fmt.Println(err)
+			logger.Error().Err(err)
 		}
 	}
 }
